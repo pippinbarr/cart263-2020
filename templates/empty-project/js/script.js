79db1676eebc12da6f667317d8b59756c0dc1b49
Pippin Barr <pippin.barr@gmail.com>
2020-01-05 12:03:46 -0500

    Added two basic templates (p5 and empty)

---
+++ templates/empty-project/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
