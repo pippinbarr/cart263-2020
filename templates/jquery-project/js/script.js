fabe0fb18269f87e617a9c2640d0b38161eaef32
Pippin Barr <pippin.barr@gmail.com>
2020-01-31 13:32:17 -0500

    Added jQuery template

---
+++ templates/jquery-project/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+}
